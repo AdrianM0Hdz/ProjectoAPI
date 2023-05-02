2e790343f7ea83f98684903f6f39452e37f14bac
AdrianM0Hdz <adrian.hernandez.p0@gmail.com>
2023-05-02 08:51:26 -0600

    added database models

--- user_resource/models.py
+++ user_resource/models.py
@@ -11,3 +11,15 @@ class PlayerUser(models.Model):
     firstname = models.CharField(max_length=32)
     lastname = models.CharField(max_length=32)
     manager = models.ForeignKey(ManagerUser, on_delete=models.CASCADE)
+
+class PartidaFallingObjects(models.Model):
+    player = models.ForeignKey(PlayerUser, on_delete=models.CASCADE)
+    score = models.IntegerField(null=False)
+    time = models.IntegerField(null=False)
+
+class PartidaEncontrarDiferencias(models.Model):
+    player = models.ForeignKey(PlayerUser, on_delete=models.CASCADE)
+    maxtime = models.IntegerField(null=False)
+    n_objects = models.IntegerField(null=False)
+    time =  models.IntegerField(null=False)
+    found_objects = models.IntegerField(null=False)    
